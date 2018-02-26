41e336ab4044f0a19b785a614fd2c8dd6e1a3844
tken-Interrobot <32670510+tken-Interrobot@users.noreply.github.com>
2018-02-26 20:14:44 +0900

    Basic fields voice input (#14)
    * Allow user, title and smile event to be voice input
    
    * Save dialogflow intents screen as reference of supported dialogs
    
    * Allow faces to be voice input
    
    It's unstable but is commited as it doesn't cause a problem with voice
    inputting other information unless face is voice input.

--- action/functions/index.js
+++ action/functions/index.js
@@ -14,34 +14,51 @@ exports.add1 = functions.https.onRequest((request, response) => {
   const app = new DialogflowApp({request, response});
   const db = admin.database();
 
-  function userForNumber (userNumber) {
-    console.log("userForNumber: " + userNumber);
-    return db.ref('users').orderByChild('number').equalTo(userNumber)
+  function userForNumber (number) {
+    console.log("userForNumber: " + number);
+    return db.ref('users').orderByChild('number').equalTo(number)
       .once('value');
   }
 
-  function patientForNumber (patientNumber) {
-    console.log("patientForNumber: " + patientNumber);
-    return db.ref('patients').orderByChild('number').equalTo(patientNumber)
+  function patientForNumber (number) {
+    console.log("patientForNumber: " + number);
+    return db.ref('patients').orderByChild('number').equalTo(number)
+      .once('value');
+  }
+
+  function faceForName (name) {
+    console.log("faceForName: " + name);
+    return db.ref('faces').orderByChild('name').equalTo(name)
       .once('value');
   }
 
   function responseHandlerAdd1 (app) {
-    console.log("8");
+    console.log("18");
 
-    const number = app.getArgument('patient_number');
+    const userNumber = app.getArgument('user_number');
+    const patientNumber = app.getArgument('patient_number');
     const event = app.getArgument('patient_event');
+    const title = app.getArgument('title');
+    const smile = app.getArgument('smile');
+    const faceName = app.getArgument('face');
 
-    const promiseUid = userForNumber(1);
-    const promisePid = patientForNumber(parseInt(number, 10));
+    const promiseUid = userForNumber(parseInt(userNumber, 10));
+    const promisePid = patientForNumber(parseInt(patientNumber, 10));
+    const promiseFid = faceName ? faceForName(faceName) : null;
+    const promiseAll = [promiseUid, promisePid, promiseFid];
 
-    return Promise.all([promiseUid, promisePid]).then((snapshots) => {
+    return Promise.all(promiseAll).then((snapshots) => {
       const users = snapshots[0];
       const patients = snapshots[1];
+      const faces = snapshots[2];
       console.log("users numChildren: " + users.numChildren());
       console.log("patients numChildren: " + patients.numChildren());
+      if (faces) {
+        console.log("faces numChildren: " + faces.numChildren());
+      }
       var uid, user;
       var pid, patient;
+      var fid, face;
       users.forEach((u) => {
         uid = u.key;
         user = u.val();
@@ -50,33 +67,45 @@ exports.add1 = functions.https.onRequest((request, response) => {
         pid = p.key;
         patient = p.val();
       });
+      if (faces) {
+        faces.forEach((f) => {
+          fid = f.key;
+          face = f.val();
+        });
+      }
       console.log("user uid: " + uid);
       console.log("user name: " + user.name);
       console.log("user number: " + user.number);
       console.log("patient pid: " + pid);
       console.log("patient name: " + patient.name);
       console.log("patient number: " + patient.number);
-      return admin.database().ref('memos').push({
+      if (fid && face) {
+        console.log("face fid: " + fid);
+        console.log("face name: " + face.name);
+      }
+      const item = {
         timestamp: admin.database.ServerValue.TIMESTAMP,
-        number: number,
+        number: patientNumber,
         event: event,
         user_id: uid,
         patient_id: pid,
-        face_confusion: true,
-        face_dependence: false,
-        face_daydream: false,
-        title: "タイトル",
+        title: title,
         event_care: event,
+        event_smile: smile,
         timestamp_evented: admin.database.ServerValue.TIMESTAMP,
         timestamp_created: admin.database.ServerValue.TIMESTAMP,
-        user_created: uid,
-      }).then(snapshot => {
+        user_created: uid, };
+      if (fid && face) { item[fid] = true; }
+      return admin.database().ref('memos').push(item).then(snapshot => {
         console.log("success.");
-        app.tell('患者さん' + number + '番の出来事、' + event + '、をメモしました。');
-      }).catch((c2) => {
-        console.log("c2");
-        console.log(c2);
-      });
+        app.tell(
+          '患者さん' + patientNumber + '番のメモ、' +
+          title + 'について、' +
+          event + '、をメモしました。');
+        }).catch((c2) => {
+          console.log("c2");
+          console.log(c2);
+        });
     }).then((t1) => {
       console.log("t1");
     }).catch((c1) => {
